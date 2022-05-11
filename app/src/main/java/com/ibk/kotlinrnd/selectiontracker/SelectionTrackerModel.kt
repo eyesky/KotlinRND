80127cc2ddfb4c4e1c59b3b3c65d838363fc2d81
imran.nirob <imran8t7@gmail.com>
2022-05-11 18:12:33 +0600

    added SimpleGenericRecyclerAdapter.kt

--- app/src/main/java/com/ibk/kotlinrnd/selectiontracker/SelectionTrackerModel.kt
+++ app/src/main/java/com/ibk/kotlinrnd/selectiontracker/SelectionTrackerModel.kt
@@ -1,9 +1,5 @@
 package com.ibk.kotlinrnd.selectiontracker
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class SelectionTrackerModel(
     val itemName: String
-) : Parcelable
+)
