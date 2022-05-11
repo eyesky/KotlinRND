547b9368806247150629067bd100ecc625126034
imran.nirob <imran8t7@gmail.com>
2022-05-11 18:11:50 +0600

    added SimpleGenericRecyclerAdapter.kt

--- app/src/main/java/com/ibk/kotlinrnd/selectiontracker/SelectionTrackerFragment.kt
+++ app/src/main/java/com/ibk/kotlinrnd/selectiontracker/SelectionTrackerFragment.kt
@@ -1,83 +1,64 @@
 package com.ibk.kotlinrnd.selectiontracker
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.CheckBox
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.selection.SelectionPredicates
 import androidx.recyclerview.selection.SelectionTracker
-import androidx.recyclerview.selection.StorageStrategy
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ibk.kotlinrnd.R
-import com.ibk.kotlinrnd.databinding.ItemSelectionTrackerBinding
-import kotlinx.android.synthetic.main.fragment_selection_tracker.*
+import com.ibk.kotlinrnd.databinding.FragmentSelectionTrackerBinding
+import com.ibk.kotlinrnd.generic.GenericSimpleRecyclerBindingInterface
+import com.ibk.kotlinrnd.generic.SimpleGenericRecyclerAdapter
 
 class SelectionTrackerFragment : Fragment(R.layout.fragment_selection_tracker) {
 
+    private lateinit var binding: FragmentSelectionTrackerBinding
     private val args: SelectionTrackerFragmentArgs by navArgs()
-    private lateinit var genericAdapter: GenericRecyclerViewAdapter<SelectionTrackerModel, ItemSelectionTrackerBinding>
-    private lateinit var selectionTracker: androidx.recyclerview.selection.SelectionTracker<Long>
+
+    private lateinit var adapter: SimpleGenericRecyclerAdapter<SelectionTrackerModel>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        genericAdapter = GenericRecyclerViewAdapter(R.layout.item_selection_tracker, createBindingInterface())
+        binding = FragmentSelectionTrackerBinding.bind(view)
 
-        selectionTracker = SelectionTracker.Builder(
-            "material",
-            rv_selection_tracker,
-            GenericItemKeyProvider(rv_selection_tracker),
-            GenericItemLookUp(rv_selection_tracker),
-            StorageStrategy.createLongStorage()
-        ).withSelectionPredicate(SelectionPredicates.createSelectAnything()).build()
+        adapter = SimpleGenericRecyclerAdapter(
+            getList(),
+            R.layout.item_selection_tracker,
+            createInterface()
+        )
 
-        selectionTracker.addObserver(object : SelectionTracker.SelectionObserver<Long>() {
-            override fun onSelectionChanged() {
-                super.onSelectionChanged()
-//                menuView.findItem(R.id.item_check).isVisible = !selectionTracker.selection.isEmpty
-                if (selectionTracker.selection.size() > 0) {
-                    selectedItems = selectionTracker.selection.map {
-                        genericAdapter.currentList[it.toInt()]
-                    }
-                    selectedItems.forEach {
-                        Log.i("items", "$it")
-                    }
-                }
-            }
+        binding.rvSelectionTracker.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        binding.rvSelectionTracker.adapter = adapter
 
-        })
-        genericAdapter.selectionTracker = selectionTracker
 
     }
 
-    private fun getList() : MutableList<SelectionTrackerModel> {
-        val data = mutableListOf<SelectionTrackerModel>().apply {
-            SelectionTrackerModel("a")
-            SelectionTrackerModel("b")
-            SelectionTrackerModel("c")
-            SelectionTrackerModel("d")
-            SelectionTrackerModel("e")
-            SelectionTrackerModel("f")
-            SelectionTrackerModel("g")
-            SelectionTrackerModel("h")
-            SelectionTrackerModel("i")
+    private fun getList(): MutableList<SelectionTrackerModel> {
+        return mutableListOf(
+            SelectionTrackerModel("a"),
+            SelectionTrackerModel("b"),
+            SelectionTrackerModel("c"),
+            SelectionTrackerModel("d"),
+            SelectionTrackerModel("e"),
+            SelectionTrackerModel("f"),
+            SelectionTrackerModel("g"),
+            SelectionTrackerModel("h"),
+            SelectionTrackerModel("i"),
             SelectionTrackerModel("j")
-        }
-        return data
+        )
     }
 
-    private fun createBindingInterface() =
-        object : GenericRecyclerBindingInterface<ItemSelectionTrackerBinding, SelectionTrackerModel> {
-            override fun bindData(
-                binder: ItemSelectionTrackerBinding,
-                model: SelectionTrackerModel,
-                clickListener: GenericClickListener<SelectionTrackerModel>?,
-                position: Int
-            ) {
-                binder.checkboxSelectionTracker.text = model.itemName
+    private fun createInterface() =
+        object : GenericSimpleRecyclerBindingInterface<SelectionTrackerModel> {
+            override fun bindData(item: SelectionTrackerModel, view: View) {
+                view.findViewById<CheckBox>(R.id.checkbox_selection_tracker).text = item.itemName
             }
 
         }
-
-
 }
+
+
