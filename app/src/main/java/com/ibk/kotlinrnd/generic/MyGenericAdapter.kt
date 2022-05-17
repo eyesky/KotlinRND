f31c63d5dd2ba8c1a80d77c6a297dab6aaafdd4c
imran.nirob <imran8t7@gmail.com>
2022-05-17 16:45:04 +0600

    working selection tracker

--- app/src/main/java/com/ibk/kotlinrnd/generic/MyGenericAdapter.kt
+++ app/src/main/java/com/ibk/kotlinrnd/generic/MyGenericAdapter.kt
@@ -1,7 +1,11 @@
 package com.ibk.kotlinrnd.generic
 
 import android.annotation.SuppressLint
+import android.view.MotionEvent
 import android.view.ViewGroup
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.selection.ItemKeyProvider
+import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -11,14 +15,9 @@ class MyGenericAdapter<T> :
     ListAdapter<T, MyGenericAdapter.ViewHolder<T>>(MyDiffUtilCallback()) {
 
     var expOnCreateViewHolder: ((ViewGroup) -> ViewBinding)? = null
-    var expOnViewHolderBinding: ((T, ViewBinding) -> Unit)? = null
+    var expOnViewHolderBinding: ((T, Int, ViewBinding) -> Unit)? = null
 
-    class ViewHolder<T>(
-        private val binding: ViewBinding,
-        private val expression: ((T, ViewBinding) -> Unit)?
-    ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: T) = expression?.let { it(item, binding) }
-    }
+    var selectionTracker: SelectionTracker<Long>? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<T> {
         return expOnCreateViewHolder?.let { it(parent) }
@@ -27,21 +26,64 @@ class MyGenericAdapter<T> :
 
     override fun onBindViewHolder(holder: ViewHolder<T>, position: Int) {
         val item = getItem(position)
-        holder.bind(item)
+        holder.bind(
+            item,
+            position,
+            selectionTracker?.isSelected(position.toLong()) ?: false
+        )
     }
 
     override fun getItemId(position: Int): Long {
         return position.toLong()
     }
 
-}
+    class ViewHolder<T>(
+        private val binding: ViewBinding,
+        private val expression: ((T, Int, ViewBinding) -> Unit)?
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: T, position: Int, b: Boolean) {
+            expression?.let { it(item, position, binding) }
+            itemView.isActivated = b
+        }
+
+        fun getItemDetails() = object :
+            ItemDetailsLookup.ItemDetails<Long>() {
+            override fun getPosition() = bindingAdapterPosition
+            override fun getSelectionKey() = itemId
+        }
+    }
 
 
-class MyDiffUtilCallback<T> : DiffUtil.ItemCallback<T>() {
-    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean =
-        oldItem.toString() == newItem.toString()
+    class MyDiffUtilCallback<T> : DiffUtil.ItemCallback<T>() {
+        override fun areItemsTheSame(oldItem: T, newItem: T): Boolean =
+            oldItem.toString() == newItem.toString()
 
-    @SuppressLint("DiffUtilEquals")
-    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean =
-        oldItem == newItem
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(oldItem: T, newItem: T): Boolean =
+            oldItem == newItem
+    }
+
+    class GenericItemKeyProvider(private val recyclerView: RecyclerView) : ItemKeyProvider<Long>(
+        SCOPE_CACHED
+    ) {
+        override fun getKey(position: Int) = recyclerView.adapter?.getItemId(position)
+
+        override fun getPosition(key: Long) =
+            recyclerView.findViewHolderForItemId(key)?.layoutPosition ?: RecyclerView.NO_POSITION
+    }
+
+    class GenericItemLookUp(private val recyclerView: RecyclerView) : ItemDetailsLookup<Long>() {
+        override fun getItemDetails(e: MotionEvent): ItemDetails<Long>? {
+            val view = recyclerView.findChildViewUnder(e.x, e.y)
+            if (view != null)
+                return (recyclerView.getChildViewHolder(view) as ViewHolder<*>).getItemDetails()
+
+            return null
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/com/ibk/kotlinrnd/selectiontracker/SelectionTrackerFragment.kt
+++ app/src/main/java/com/ibk/kotlinrnd/selectiontracker/SelectionTrackerFragment.kt
@@ -1,10 +1,14 @@
 package com.ibk.kotlinrnd.selectiontracker
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.selection.SelectionPredicates
+import androidx.recyclerview.selection.SelectionTracker
+import androidx.recyclerview.selection.StorageStrategy
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ibk.kotlinrnd.R
 import com.ibk.kotlinrnd.databinding.FragmentSelectionTrackerBinding
@@ -12,10 +16,12 @@ import com.ibk.kotlinrnd.databinding.ItemSelectionTrackerBinding
 import com.ibk.kotlinrnd.generic.MyGenericAdapter
 
 class SelectionTrackerFragment : Fragment(R.layout.fragment_selection_tracker) {
-
+    private lateinit var selectionItems: List<SelectionTrackerModel>
+    private val TAG = "SelectionTrackerFragmen"
     private lateinit var binding: FragmentSelectionTrackerBinding
     private val args: SelectionTrackerFragmentArgs by navArgs()
     private lateinit var mAdapter: MyGenericAdapter<SelectionTrackerModel>
+    private lateinit var selectionTracker: SelectionTracker<Long>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -25,10 +31,14 @@ class SelectionTrackerFragment : Fragment(R.layout.fragment_selection_tracker) {
         //Initiate the adapter with type
         mAdapter = MyGenericAdapter()
 
-        mAdapter.expOnViewHolderBinding = { item, viewBinding ->
+        mAdapter.expOnViewHolderBinding = { item, pos, viewBinding ->
             //cast the viewBinding with your layout binding class
             var binding = viewBinding as ItemSelectionTrackerBinding
             binding.checkboxSelectionTracker.text = item.itemName
+            binding.checkboxSelectionTracker.setOnClickListener {
+                Log.e(TAG, "onViewCreated: ${item.itemName} pos: $pos")
+            }
+
         }
 
         //Inflate the layout and send it to the adapter
@@ -41,6 +51,31 @@ class SelectionTrackerFragment : Fragment(R.layout.fragment_selection_tracker) {
             layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
             adapter = mAdapter
         }
+
+        selectionTracker = SelectionTracker.Builder(
+            "material",
+            binding.rvSelectionTracker,
+            MyGenericAdapter.GenericItemKeyProvider(binding.rvSelectionTracker),
+            MyGenericAdapter.GenericItemLookUp(binding.rvSelectionTracker),
+            StorageStrategy.createLongStorage()
+        ).withSelectionPredicate(SelectionPredicates.createSelectAnything()).build()
+
+        selectionTracker.addObserver(object : SelectionTracker.SelectionObserver<Long>(){
+            override fun onSelectionChanged() {
+                super.onSelectionChanged()
+                if (selectionTracker.selection.size() > 0) {
+                    selectionItems = selectionTracker.selection.map {
+                        mAdapter.currentList[it.toInt()]
+                    }
+                    selectionItems.forEach{
+                        Log.e(TAG, "onSelectionChanged: ${it.itemName}")
+                    }
+                }
+            }
+        })
+
+        mAdapter.selectionTracker = selectionTracker
+
 
         // here pass the array list data into adapter
         mAdapter.submitList(getList())
